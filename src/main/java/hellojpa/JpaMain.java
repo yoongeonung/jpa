387138321b800ae384e066b9a2547994484e5929
yoongeonung <yoongeonung@gmail.com>
2021-10-16 12:30:27 +0900

    feat : Entity Lifecycle

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,9 @@
 package hellojpa;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -11,7 +13,14 @@ public class JpaMain {
         EntityTransaction transaction = manager.getTransaction();
         transaction.begin();
         try {
-            manager.persist(new Member("Kakao"));
+            // 비영속 상태
+            Member kakao = new Member("Kakao");
+            // 영속화
+            manager.persist(kakao);
+            // 준영속, 한번 영속화 됐던 엔티티를 해제시키는것
+            manager.detach(kakao);
+            // 삭제
+            manager.remove(kakao);
             //commit
             transaction.commit();
         }catch (Exception e) {
