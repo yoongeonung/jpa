8014e57bb92d824c0a908d3aef80609c76519fe5
yoongeonung <yoongeonung@gmail.com>
2021-10-28 17:16:22 +0900

    feat : @OneToOne One-way

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,14 +14,16 @@ public class JpaMain {
         transaction.begin();
         try {
             // 비영속 상태
-            Member member = new Member();
-            member.setName("OneToMany");
-            manager.persist(member);
+            Locker locker = new Locker();
+            locker.setName("1000엔락커");
+            System.out.println("<------------------");
+            manager.persist(locker);
+            System.out.println("------------------>");
 
-            Team team = new Team();
-            team.setName("OneToManyTeam");
-            team.getMembers().add(member);
-            manager.persist(team);
+            Member member = new Member();
+            member.setName("OneToOne");
+            member.setLocker(locker); // 객체 지향 관점
+            manager.persist(member);
 
             //commit
             System.out.println("<------------------");
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,9 +10,13 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor
 public class Member {
+    // identity 전략 -> persist시 쿼리를 날려버린다. jpa가 id값을 알아야하기 때문에
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
     public Member(String name) {
         this.name = name;
     }
