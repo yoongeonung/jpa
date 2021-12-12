686c0d5c92db4946c9833469858e1c5efa25e5ed
yoongeonung <yoongeonung@gmail.com>
2021-12-12 10:22:49 +0900

    feat : MemberService

--- jpa_practice1/src/main/java/jp/ac/hal/yoongeonung/jpa_practice1/repository/MemberRepository.java
+++ jpa_practice1/src/main/java/jp/ac/hal/yoongeonung/jpa_practice1/repository/MemberRepository.java
@@ -1,23 +1,27 @@
 package jp.ac.hal.yoongeonung.jpa_practice1.repository;
 
 import jp.ac.hal.yoongeonung.jpa_practice1.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
     /*
-    @PersistenceContext를 사용하면
+    @PersistenceContext를 사용해야지만
     EntityManager를 스프링이 주입해준다.
     factory가 필요할 경우
     @PersistenceUnit 를 사용하면 된다.
+
+    현시점 spring data jpa를 사용하면 @PersistenceContext가 아닌
+    @Autowired를 사용해도 EntityManager를 주입해준다.
+    즉 @RequiredArgsConstructor를 사용 할수 있다는 뜻
      */
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
+
     public void save(Member member) {
         em.persist(member);
     }
