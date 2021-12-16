e86ae2024f4032f0c59e4f7d1df827f853d7b029
yoongeonung <yoongeonung@gmail.com>
2021-12-16 18:48:34 +0900

    feat : HomeController & CSS Markup

--- jpa_practice1/src/main/java/jp/ac/hal/yoongeonung/jpa_practice1/repository/OrderRepository.java
+++ jpa_practice1/src/main/java/jp/ac/hal/yoongeonung/jpa_practice1/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,8 +17,17 @@ public class OrderRepository {
         em.persist(order);
     }
 
+    // 단건 조회
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
+    //
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // TODO 1. 쿼리dsl로 변환
+        return em.createQuery("select o from Order o where o.status = :status and o.member.name = :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .getResultList();
+    }
 }
