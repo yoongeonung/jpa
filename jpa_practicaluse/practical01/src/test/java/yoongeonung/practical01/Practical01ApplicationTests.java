b632c06a524d4b2680ad2d72bb48d44b40d1225e
yoongeonung <yoongeonung@gmail.com>
2022-03-05 00:03:03 +0900

    feat : Jpa Practical 01 Project Setup

---
+++ jpa_practicaluse/practical01/src/test/java/yoongeonung/practical01/Practical01ApplicationTests.java
@@ -0,0 +1,13 @@
+package yoongeonung.practical01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practical01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
