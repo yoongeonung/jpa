610924892bde8b794a89da110a3215f43336d2b7
yoongeonung <yoongeonung@gmail.com>
2022-05-11 17:59:46 +0900

    WEBAPP : MemberRepository

---
+++ webapp/src/main/java/yoongeonung/webapp/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package yoongeonung.webapp.repository;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import yoongeonung.webapp.domain.Member;
+
+@Repository
+public class MemberRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  public void save(Member member) {
+    entityManager.persist(member);
+  }
+  //  TODO 2. 옵셔널 적용해보기
+  public Member findOne(Long id) {
+    return entityManager.find(Member.class, id);
+  }
+
+  public List<Member> findAll() {
+    return entityManager.createQuery("select m from Member m", Member.class).getResultList();
+  }
+
+//  TODO 1. 페이징 적용해보기
+  public List<Member> findByName(String name) {
+    return entityManager.createQuery("select m from Member m where m.name = :name", Member.class)
+        .setParameter("name", name)
+        .getResultList();
+  }
+}
