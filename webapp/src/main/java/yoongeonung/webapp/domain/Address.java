c240b33ca7711fd1f3b6abd23849b3f33ba7ff31
yoongeonung <yoongeonung@gmail.com>
2022-05-17 09:44:50 +0900

    WEBAPP : FIN

--- webapp/src/main/java/yoongeonung/webapp/domain/Address.java
+++ webapp/src/main/java/yoongeonung/webapp/domain/Address.java
@@ -3,16 +3,18 @@ package yoongeonung.webapp.domain;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class Address {
   @Column(length = 50)
   private String city;
   @Column(length = 50)
-  private String string;
+  private String street;
   @Column(length = 50)
   private String zipcode;
 }
---
+++ webapp/src/main/java/yoongeonung/webapp/web/MemberController.java
@@ -0,0 +1,49 @@
+package yoongeonung.webapp.web;
+
+import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import yoongeonung.webapp.domain.Address;
+import yoongeonung.webapp.domain.Member;
+import yoongeonung.webapp.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+  private final MemberService memberService;
+
+  @GetMapping("/members/new")
+  public String createForm(Model model) {
+
+    model.addAttribute("memberForm", new MemberForm());
+    return "members/createMemberForm";
+  }
+
+  @PostMapping(value = "/members/new")
+  public String create(@Valid MemberForm form, BindingResult result) {
+    if (result.hasErrors()) {
+      return "members/createMemberForm";
+    }
+
+    Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+    Member member = Member.create(form.getName(), address);
+
+    memberService.join(member);
+    return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+
+    return "members/memberList";
+  }
+}
